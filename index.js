f9bc116454603b31ecba59a2900b68c66e448f69
Felipe Maximiano <maximiano.felipesilva@gmail.com>
2022-01-28 22:55:10 -0300

    Atualização

--- index.js
+++ index.js
@@ -6,94 +6,100 @@ console.log("\n O jogo será Jokenpô, Para tomar suas decisões, utilize 1 para
 let inicio = prompt("Deseja iniciar o jogo? ");
 if(isNaN(inicio))
 {
-while(inicio.toUpperCase() == 'S' || inicio.toUpperCase() == 'SIM')
-{
-let vitjog = 0;
-let vitCOM = 0;
-let rodadas = prompt("Defina o número de rodadas que deseja jogar: ");
-while(isNaN(rodadas))
-{
-    console.log("Digito inválido, selecione um número de rodadas!");
-    rodadas = prompt("Defina o número de rodadas que deseja jogar: ");
-}
-for(i = 0; i < rodadas; i++)
-{
-    console.log(`\nInicio da rodada ${i + 1}`);
-    let jogador1 = prompt("Jogador, selecione o que irá jogar: ");
-    if(jogador1 >= 1 && jogador1 <= 3 )
+    if(inicio.toUpperCase() == 'S' || inicio.toUpperCase() == 'SIM')
     {
-        console.log(`\nJogador selecionou ${escolhas[jogador1 - 1]} \n`);
-        let COM = Math.floor(Math.random() * 3) + 1;
-        console.log(`Computador selecionou ${escolhas[COM - 1]} \n`);
-        if (jogador1 == COM)
+    while(inicio.toUpperCase() == 'S' || inicio.toUpperCase() == 'SIM')
+    {
+    let vitjog = 0;
+    let vitCOM = 0;
+    let rodadas = prompt("Defina o número de rodadas que deseja jogar: ");
+    while(isNaN(rodadas))
+    {
+        console.log("Digito inválido, selecione um número de rodadas!");
+        rodadas = prompt("Defina o número de rodadas que deseja jogar: ");
+    }
+    for(i = 0; i < rodadas; i++)
+    {
+        console.log(`\nInicio da rodada ${i + 1}`);
+        let jogador1 = prompt("Jogador, selecione o que irá jogar: ");
+        if(jogador1 >= 1 && jogador1 <= 3 )
         {
-            console.log("\nEmpate");
-        }
-        else if (jogador1 == 1)
-        {
-            if(COM == 2)
+            console.log(`\nJogador selecionou ${escolhas[jogador1 - 1]} \n`);
+            let COM = Math.floor(Math.random() * 3) + 1;
+            console.log(`Computador selecionou ${escolhas[COM - 1]} \n`);
+            if (jogador1 == COM)
             {
-                console.log("\nComputador venceu a rodada!\n");
-                vitCOM++;
+                console.log("\nEmpate");
+            }
+            else if (jogador1 == 1)
+            {
+                if(COM == 2)
+                {
+                    console.log("\nComputador venceu a rodada!\n");
+                    vitCOM++;
+                }
+                else
+                {
+                    console.log("\nJogador venceu a rodada!\n");
+                    vitjog++;
+                }
+            }
+            else if (jogador1 == 2)
+            {
+                if(COM == 3)
+                {
+                    console.log("\nComputador venceu a rodada!\n");
+                    vitCOM++;''
+                }
+                else
+                {
+                    console.log("\nJogador venceu a rodada!\n");
+                    vitjog++;
+                }
             }
             else
             {
-                console.log("\nJogador venceu a rodada!\n");
-                vitjog++;
-            }
-        }
-        else if (jogador1 == 2)
-        {
-            if(COM == 3)
-            {
-                console.log("\nComputador venceu a rodada!\n");
-                vitCOM++;''
-            }
-            else
-            {
-                console.log("\nJogador venceu a rodada!\n");
-                vitjog++;
+                if(COM == 1)
+                {
+                    console.log("\nComputador venceu a rodada!\n");
+                    vitCOM++;
+                }
+                else
+                {
+                    console.log("\nJogador venceu a rodada!\n");
+                    vitjog++;
+                }
             }
         }
         else
         {
-            if(COM == 1)
-            {
-                console.log("\nComputador venceu a rodada!\n");
-                vitCOM++;
-            }
-            else
-            {
-                console.log("\nJogador venceu a rodada!\n");
-                vitjog++;
-            }
+            console.log("\nOpção inválida jogador, escolha novamente\n");
+            i--;
         }
     }
+    console.log("\n");
+    console.log(`\nA partida ficou JOGADOR ${vitjog} x ${vitCOM} COMPUTADOR\n`)
+    if(vitjog > vitCOM)
+        {
+            console.log("\nO JOGADOR FOI O GRANDE VENCEDOR!\n");
+        }
+    else if(vitCOM > vitjog)
+        {
+            console.log("\nO COMPUTADOR FOI O GRANDE VENCEDOR!\n");
+        }
     else
+        {
+            console.log("\nHOUVE UM EMPATE NA PARTIDA\n");
+        }
+        inicio = prompt("Deseja jogar novamente? ");
+    }
+    }
+    else if(inicio.toUpperCase() == 'N' || inicio.toUpperCase() == 'NAO')
     {
-        console.log("\nOpção inválida jogador, escolha novamente\n");
-        i--;
+        console.log("\nO Jogo acabou!");
     }
 }
-console.log("\n");
-console.log(`\nA partida ficou JOGADOR ${vitjog} x ${vitCOM} COMPUTADOR\n`)
-if(vitjog > vitCOM)
-    {
-        console.log("\nO JOGADOR FOI O GRANDE VENCEDOR!\n");
-    }
-else if(vitCOM > vitjog)
-    {
-        console.log("\nO COMPUTADOR FOI O GRANDE VENCEDOR!\n");
-    }
-else
-    {
-        console.log("\nHOUVE UM EMPATE NA PARTIDA\n");
-    }
-    inicio = prompt("Deseja jogar novamente? ");
-}
-}
-else
+else if(!isNaN(inicio))
 {
     console.log("Parametro inválido!\nO jogo será encerrado.");
 }
-console.log("\nO Jogo acabou!");
